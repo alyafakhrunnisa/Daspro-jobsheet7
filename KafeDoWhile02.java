f2489e25966b7e7d6681611add1e8c3554dfee06
alyafakhrunnisa <nisafakhrunalya@gmail.com>
2025-10-22 10:23:57 +0700

    Percobaan3

---
+++ KafeDoWhile02.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class KafeDoWhile02 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int kopi,teh,roti;
+        int hargaKopi = 12000;
+        int hargaTeh = 7000;
+        int hargaRoti = 20000;
+        int totalHarga = 0;
+        String namaPelanggan;
+
+        do{
+            System.out.print("Masukkan nama pelanggan (ketik 'batal untuk keluar'): ");
+            namaPelanggan = sc.nextLine();
+
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Transaksi dibatalkan.");
+                break;
+            }
+            System.out.print("Jumlah kopi: ");
+            kopi = sc.nextInt();
+            System.out.print("Jumlah teh: ");
+            teh = sc.nextInt();
+            System.out.print("Jumlah roti: ");
+            roti = sc.nextInt();
+
+            totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+            System.out.println("Total harga yang harus dibayar : Rp " + totalHarga);
+            sc.nextLine(); // membersihkan buffer
+        } while (true);
+        System.out.println("Semua transaksi selesai.");
+    }
+}
