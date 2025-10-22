666737722741a1ab523567e5b46d8dc461c0425d
alyafakhrunnisa <nisafakhrunalya@gmail.com>
2025-10-22 08:49:17 +0700

    modifikasi1

--- SiakadFor02.java
+++ SiakadFor02.java
@@ -5,19 +5,30 @@ public class SiakadFor02 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Masukkan nilai mahasiswa ke-" + i + ": ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
-            
+            }
+
+             // hitung lulus dan tidak lulus
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
+        
+         // tampilkan hasil
         System.out.println("Nilai tertinggi adalah: " + tertinggi);
         System.out.println("Nilai terendah adalah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
     }
-}
 }
